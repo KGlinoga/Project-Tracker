294dcf996ca84a10fe85b30ef3b132bf3b2cc2ba
LindsayLew <lmschwehr@gmail.com>
2022-07-01 13:35:25 -0700

    attempting to create and append a new <td> element to the project rows when more than one project exists

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,6 +4,25 @@ var tableDetail = document.querySelectorAll("<td>");
 
 var tableBody = document.querySelector("<tbody>");
 
+// appending the td elements to the table row
+    var completeRow = tableDetail.append(tableRow);
+    console.log(completeRow.textContent);
+// appending the row with appended elements to the tbody element
+    var combinedBody = completeRow.append(tableBody);
 
-tableDetail.append(tableBody)
-console.log()
+    //when user creates a new row, need to add a td for a button for deletions
+//new row with new project
+
+    //button to add project in the card would trigger this creation of button deletions if one already exists --so new project would be attached to the button, firstProject is the original button input
+newProject = tableDetail.oninput = function(){
+    oninput = document.createElement("<tr>");
+        return() var deleteDetail = "<tr>"
+    newProject.addEventListener("input",(firstProject) {
+        deleteDetail.append(tableDetail)
+    });
+}
+        
+ 
+        //user input for adding a new o upon this create element
+     
+
