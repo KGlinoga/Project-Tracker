f969262b78e1e2ad36e3c5e67d601dfea8d9b4a4
LindsayLew <lmschwehr@gmail.com>
2022-07-01 12:44:50 -0700

    appending

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,9 @@
 //js file
+var tableRow = document.querySelectorAll("<tr>");
+var tableDetail = document.querySelectorAll("<td>");
+
+var tableBody = document.querySelector("<tbody>");
+
+
+tableDetail.append(tableBody)
+console.log()
